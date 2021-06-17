9d36327efed1dd5f19af9ddf3854206ac81ff42e
何飞 <504479518@qq.com>
2021-06-18 01:20:03 +0800

    堆空间设置大小

--- chapter08/src/com/caesar/java/HeapSpaceInitial.java
+++ chapter08/src/com/caesar/java/HeapSpaceInitial.java
@@ -2,19 +2,40 @@ package com.caesar.java;
 
 /**
  * @author Nicolas Caesar
+ * 1.设置堆空间大小的参数
+ * -Xms 用来设置堆空间（年轻代与老年代）的初始内存大小
+ * -X 是jvm的运行参数
+ * ms 是memory start
+ * -Xmx 用来设置堆空间（年轻代与老年代）的最大内存大小
+ * <p>
+ * 2.默认堆空间的大小
+ * 初始内存大小：物理电脑内存大小 /64
+ * -Xms : 243M
+ * 最大内存大小：物理电脑内存大小 /4
+ * -Xmx : 3602M
+ * 3.手动设置：-Xms600m -Xmx600m
+ *  开发中建议将初始堆内存和最大的堆内存设置成相同的值
+ *
+ * 4.查看设置的参数：方式一：jps    / jstat -gc 进程id（该命令为jdk的bin下的指令在控制台窗口查看）
+ *                方式二：-xx:printGCdetails (在idea的vm参数中添加)
  */
 public class HeapSpaceInitial {
 
     public static void main(String[] args) {
         // 返回java虚拟机中的堆内存总量
-        long initialMemory = Runtime.getRuntime().totalMemory();
+        long initialMemory = Runtime.getRuntime().totalMemory() / 1024 / 1024;
         // 返回java虚拟机视图使用的最大堆内存量
-        long maxMemory = Runtime.getRuntime().maxMemory();
+        long maxMemory = Runtime.getRuntime().maxMemory()/ 1024 / 1024;
 
         System.out.println("-Xms : " + initialMemory + "M");
         System.out.println("-Xmx : " + maxMemory + "M");
 
-        System.out.println("系统内存大小为：" + initialMemory * 64.0 / 1024 + "G");
-        System.out.println("系统内存大小为：" + maxMemory * 4.0 / 1024 + "G");
+//        System.out.println("系统内存大小为：" + initialMemory * 64.0 / 1024 + "G");
+//        System.out.println("系统内存大小为：" + maxMemory * 4.0 / 1024 + "G");
+        try {
+            Thread.sleep(1000000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
